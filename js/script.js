e2fc4b27a441d795b950769536dbf74fe18d9e92
sisdream <sissipascale2@gmail.com>
2024-01-11 15:45:11 +0100

    feat: css

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 const divSquare = document.getElementById("block-container");
+
 // ciclo per creare i numeri
 
 for (let i = 1; i < 101; i++){
-    console.log(i)
     const square = document.createElement("div");
     square.classList.add("square");
-    console.log(square);
+    divSquare.append("square");
+    
+    if(i % 15 == 0){
+        console.log("FizzBuzz");
+    }else if(i % 3 == 0){
+        console.log("Fizz");
+    }else if(i % 5 == 0){
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
 };
