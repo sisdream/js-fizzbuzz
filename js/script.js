c35dd83f4871c8914fb99fe3b9e04d65d3d32421
sisdream <sissipascale2@gmail.com>
2024-01-11 16:16:26 +0100

    fix: ciclo

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
-const divSquare = document.getElementById("block-container");
+const divSquare = document.getElementById('block-container');
 
-// ciclo per creare i numeri
+// creazione di ciclo
 
-for (let i = 1; i < 101; i++){
-    const square = document.createElement("div");
-    square.classList.add("square");
-    divSquare.append("square");
-    
-    if(i % 15 == 0){
-        console.log("FizzBuzz");
-    }else if(i % 3 == 0){
-        console.log("Fizz");
-    }else if(i % 5 == 0){
-        console.log("Buzz");
-    }else{
-        console.log(i);
+for (let i = 1; i <=100; i++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    divSquare.append(square);
+
+    if(i % 15 === 0) {
+        square.classList.add('fizzBuzz');
+        square.append("FizzBuzz");
+    }else if(i % 5 === 0) {
+        square.classList.add('divBuzz');
+        square.append("Buzz")
+    }else if (i % 3 === 0) {
+        square.classList.add('divFizz');
+        square.append("Fizz");
+    }else {
+        square.append(i);
     }
-};
+}
